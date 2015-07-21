55909be5aed3a6fee9999a2933cb352e682ec277
MATSUU Takuto <matsuu@gmail.com>
2015-07-22 06:55:35 +0900

    set sha256 as Signature Algorithm

--- gencert.sh
+++ gencert.sh
@@ -5,4 +5,4 @@ See https://github.com/frntn/x509-san/blob/master/README.md
 USAGE
 
 certname="frntn-x509-san"
-openssl x509 -in <(openssl req -days 3650 -newkey rsa:4096 -nodes -keyout "${certname}.key" -subj "/C=${CRT_C:-"FR"}/L=${CRT_L:-"Paris"}/O=${CRT_O:-"Ekino"}/OU=${CRT_OU:-"DevOps"}/CN=${CRT_CN:-"base.example.com"}") -req -signkey "${certname}.key" -days 3650 -out "${certname}.crt" -extfile <(echo "subjectAltName=${CRT_SAN:-"DNS.1:logs.example.com,DNS.2:metrics.example.com,IP.1:192.168.0.1,IP.2:10.0.0.50"}")
+openssl x509 -in <(openssl req -days 3650 -newkey rsa:4096 -nodes -keyout "${certname}.key" -subj "/C=${CRT_C:-"FR"}/L=${CRT_L:-"Paris"}/O=${CRT_O:-"Ekino"}/OU=${CRT_OU:-"DevOps"}/CN=${CRT_CN:-"base.example.com"}") -req -signkey "${certname}.key" -sha256 -days 3650 -out "${certname}.crt" -extfile <(echo "subjectAltName=${CRT_SAN:-"DNS.1:logs.example.com,DNS.2:metrics.example.com,IP.1:192.168.0.1,IP.2:10.0.0.50"}")
